e4be760ec7198d0c1207efe021086f19f91c06ce
laixiaobo <1483628145l@gmail.com>
2026-02-05 10:24:34 +0800

    element-plus引入

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 
 <template>
   <div>Hello Vue3</div>
+  <el-button type="primary">全局按钮</el-button>
 </template>
 
 <style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// element-plus 引入
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 中文
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+// 使用element-plus
+app.use(ElementPlus)
 
 app.mount('#app')
