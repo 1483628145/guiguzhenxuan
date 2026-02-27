b4948d1beabb37b1d0a985733591e416584a2fb6
laixiaobo <1483628145l@gmail.com>
2026-02-27 14:42:17 +0800

    完成左侧菜单静态搭建

---
+++ src/layout/components/logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="logo">
+    <img src="../../../public/logo.png" alt="" />
+    <span>硅谷甄选平台</span>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.logo {
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 50px;
+  // background-color: pink;
+  padding: 0 20px;
+  img {
+    width: 40px;
+    height: 40px;
+  }
+
+  span {
+    font-size: $base-logo-title-fontSize;
+    color: white;
+    margin-left: 20px;
+  }
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,27 @@
 <template>
   <div class="container">
     <!-- 左侧导航 -->
-    <div class="layout-slider">1</div>
+    <div class="layout-slider">
+      <logo></logo>
+      <el-scrollbar class="scrollbar">
+        <!-- 左侧菜单- 静态 -->
+        <el-menu
+          class="el-menu-vertical-demo"
+          background-color="$base-menu-background"
+          text-color="#fff"
+        >
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <el-sub-menu index="3">
+            <template #title>权限管理</template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="4">商品管理</el-menu-item>
+        </el-menu>
+      </el-scrollbar>
+    </div>
     <!-- 顶部导航 -->
     <div class="layout-topnav">2</div>
     <!-- 内容区 -->
@@ -9,18 +29,26 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import logo from './components/logo.vue'
+</script>
 
 <style scoped lang="scss">
 .container {
   width: 100%;
   height: 100vh;
-  background-color: pink;
+  // background-color: pink;
 
   .layout-slider {
     width: $base-menu-width;
     height: 100vh;
     background-color: $base-menu-background;
+    color: white;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
 
   .layout-topnav {
